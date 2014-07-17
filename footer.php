ef8465d400ff232f3d751b3ea678d62d4f6069da
Marco Tulio <mtuliobarbosa@gmail.com>
2014-07-17 02:16:43 -0300

    Adicionar date e criar timezone

--- footer.php
+++ footer.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Menu</title>
-</head>
-
-<body>
-	<ul>
-    	<li><a href="index.php">Home</a></li>
-        <li><a href="contato.php">Contato</a></li>
-        
-    </ul>
-</body>
-</html>
+<?php date_default_timezone_set('America/Sao_Paulo'); ?>
+<footer>
+	<div class="container">
+		<p>Todos os direitos reservados - <?php echo date('Y');?></p>
+    </div>
+</footer>
