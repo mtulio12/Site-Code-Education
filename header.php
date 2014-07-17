c67cce86b7fd5cedb64ae8cf0ba72e6218a65d18
Marco Tulio <mtuliobarbosa@gmail.com>
2014-07-16 23:48:18 -0300

    Iniciando o projeto

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" media="all" href="css/bootstrap.css" />
+<title>Menu</title>
+</head>
+
+<body>
+	<ul>
+    	<li><a href="index.php">Home</a></li>
+        <li><a href="">Empresa</a></li>
+        <li><a href="">Produtos</a></li>
+        <li><a href="">Serviços</a></li>
+        <li><a href="contato.php">Contato</a></li>     
+    </ul>
+</body>
+</html>
